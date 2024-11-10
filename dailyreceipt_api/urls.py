46053e709f45e96045011edb4546e1272057478e
Madang Garden <chooi9522@gmail.com>
2024-11-10 17:31:23 +0900

    edit urls

--- dailyreceipt_api/urls.py
+++ dailyreceipt_api/urls.py
@@ -15,9 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.http import HttpResponse
 from django.contrib import admin
 from django.urls import path
 
+
+def index(request):
+    return HttpResponse("Welcome to DailyReceipt API")
+
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", index),
 ]
